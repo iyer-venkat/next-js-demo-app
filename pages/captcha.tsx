6edd19f545b075eeb389dc79294827ac320b2555
iyer-venkat <iyer_venkat@outlook.com>
2023-08-17 13:09:47 +1000

    disable v2

--- pages/captcha.tsx
+++ pages/captcha.tsx
@@ -1,44 +1,44 @@
-import ReCaptcha, { ReCAPTCHA, ReCAPTCHAProps } from "react-google-recaptcha";
-import useAxios from "axios-hooks";
-import { useRef } from "react";
+// import ReCaptcha, { ReCAPTCHA, ReCAPTCHAProps } from "react-google-recaptcha";
+// import useAxios from "axios-hooks";
+// import { useRef } from "react";
 import { GoogleReCaptchaProvider } from "react-google-recaptcha-v3";
-import styles from "../styles/Home.module.css";
+// import styles from "../styles/Home.module.css";
 import EnquiryForm from "@/components/Form";
 
 const Recaptcha = () => {
-  const url =
-    "https://www.google.com/recaptcha/api/siteverify?secret=";
-  const [reCaptchaProps, verifyCaptcha] = useAxios(
-    {
-      url,
-      method: "POST",
-    },
-    {
-      manual: true,
-    }
-  );
+  // const url =
+  //   "https://www.google.com/recaptcha/api/siteverify?secret=";
+  // const [reCaptchaProps, verifyCaptcha] = useAxios(
+  //   {
+  //     url,
+  //     method: "POST",
+  //   },
+  //   {
+  //     manual: true,
+  //   }
+  // );
 
-  const captchaRef = useRef<ReCAPTCHA>(null);
+  // const captchaRef = useRef<ReCAPTCHA>(null);
 
-  const verifyToken = async (token: string | null) => {
-    console.log("verifyToken", token);
+  // const verifyToken = async (token: string | null) => {
+  //   console.log("verifyToken", token);
 
-    await verifyCaptcha({
-      url: `${url}&response=${token}`,
-    });
-  };
+  //   await verifyCaptcha({
+  //     url: `${url}&response=${token}`,
+  //   });
+  // };
 
-  console.log(reCaptchaProps);
+  // console.log(reCaptchaProps);
   return (
     <>
-      <ReCaptcha
+      {/* <ReCaptcha
         sitekey=""
         ref={captchaRef}
         onChange={verifyToken}
-      />
+      /> */}
       <br />
       <GoogleReCaptchaProvider
-        reCaptchaKey=""
+        reCaptchaKey="6LcxNl8nAAAAAKbnfk2uyNd4AjtJX8z2LujLUyop"
         scriptProps={{
           async: false,
           defer: false,
